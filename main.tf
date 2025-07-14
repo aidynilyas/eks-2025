dc97d1e50c0fa628ceb7a85b5bb07382106b183d
aidynilyas <aidynilyas@gmail.com>
2025-07-14 14:11:07 -0400

    deploying EKS

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"][count.index]
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
+
+  cluster_name    = "example"
+  cluster_version = "1.31"
+
+  # Optional
+  cluster_endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  vpc_id     = aws_vpc.main.id
+subnet_ids = [for subnet in aws_subnet.main : subnet.id]
+
+  tags = {
+    Name = "eks"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "kubeconfig_command" {
+  value = "aws eks update-kubeconfig --region us-east-1 --name example"
+}
